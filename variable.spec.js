7c4d2316f55e9c284e72d9c97fcc1bf1f440564b
satishmore-automation <satishmore-automation@github.com>
2026-05-08 15:53:17 +0530

    varaibles type added

--- variable.spec.js
+++
@@ -1,9 +0,0 @@
-var name="John";
-var age=30;
-var isStudent=true; 
-var name="Jane"; // Redeclaring a var variable
-
-let city="New York";
-let score=85;
-let isActive=false;
-// let city="Los Angeles"; // This will cause an error because 'city' is already declared
---
+++ variableTypes.js
@@ -0,0 +1,5 @@
+// JavaScript variable types examples   
+const name = "Alice"; // String
+const age = 30; // Number
+const isStudent = false; // Boolean
+const hobbies = ["reading", "traveling", "coding"];
