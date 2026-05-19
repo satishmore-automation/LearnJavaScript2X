102cb21c14d74395f3728423bc1e798a93d40a29
satishmore-automation <satishmore-automation@github.com>
2026-05-19 18:23:45 +0530

    function demo

---
+++ jsfunctions.js
@@ -0,0 +1,23 @@
+// function add(a,b){
+//     console.log(a+b);}
+
+//     add(5,10);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// let sum = add(5,10);
+
+// console.log(sum);
+
+// function greeting(){
+//     console.log("Hello World");
+// }
+// greeting();
+
+function greeting(){
+    return "Hello World";
+}
+
+console.log(greeting());
